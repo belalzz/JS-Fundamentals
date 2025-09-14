7d190b6f4ff459247c86515924ebd2fb36d34a35
belalzz <belal.khaled041@gmail.com>
2025-09-14 18:34:03 +0300

    added task-10

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+const input = process.argv[2];
+const num = parseInt(input);
+
+function factorial(n){
+    if (isNaN(n) || n === 0) return 1;
+    else{
+        return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(num));
